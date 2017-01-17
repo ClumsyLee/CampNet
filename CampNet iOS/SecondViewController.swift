672dfe679c953bc8dd3a0f318adc302afee7d61e
Sihan Li <lisihan969@gmail.com>
2017-01-17 19:42:38 +0800

    Initial Commit

---
+++ CampNet iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CampNet iOS
+//
+//  Created by Thomas Lee on 2017/1/17.
+//  Copyright © 2017年 Sihan Li. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
