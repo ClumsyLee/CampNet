a3cbd2f59948f5801bb9a72882588a32d0203adb
Sihan Li <lisihan969@gmail.com>
2017-01-29 02:14:40 +0800

    Implement Account

---
+++ CampNet iOS/Account.swift
@@ -0,0 +1,72 @@
+//
+//  Account.swift
+//  CampNet iOS
+//
+//  Created by Thomas Lee on 2017/1/29.
+//  Copyright © 2017年 Sihan Li. All rights reserved.
+//
+
+import Foundation
+import KeychainAccess
+
+extension String  {
+    var md5: String {
+        let length = Int(CC_MD5_DIGEST_LENGTH)
+        var digest = [UInt8](repeating: 0, count: length)
+        
+        if let d = self.data(using: String.Encoding.utf8) {
+            _ = d.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
+                CC_MD5(body, CC_LONG(d.count), &digest)
+            }
+        }
+
+        return (0..<length).reduce("") {
+            $0 + String(format: "%02x", digest[$1])
+        }
+    }
+}
+
+class Account: CustomStringConvertible {
+    static let keychain = Keychain(service: Bundle.main.bundleIdentifier!)
+
+    let configurationIdentifier: String
+    let configuration: Configuration
+    let username: String
+    var password: String {
+        get { return Account.keychain[identifier] ?? "" }
+        set { Account.keychain[identifier] = newValue }
+    }
+    var identifier: String { return "\(configurationIdentifier).\(username)" }
+    var placeholders: [String: String] {
+        return [
+            "username": username,
+            "password": password,
+            "password.md5": password.md5
+        ]
+    }
+    var description: String { return "Account(\(identifier))" }
+    
+    init?(configurationIdentifier: String, username: String) {
+        self.configurationIdentifier = configurationIdentifier
+        guard let configuration = Configuration(identifier: self.configurationIdentifier) else {
+            return nil
+        }
+        self.configuration = configuration
+        self.username = username
+    }
+    
+    func login(session: URLSession, completionHandler: @escaping (NetworkAction.Result) -> Void) {
+        print("Login for \(self).")
+        configuration.loginAction.commit(placeholders: placeholders, session: session, completionHandler: completionHandler)
+    }
+    
+    func status(session: URLSession, completionHandler: @escaping (NetworkAction.Result) -> Void) {
+        print("Check status for \(self).")
+        configuration.statusAction.commit(placeholders: placeholders, session: session, completionHandler: completionHandler)
+    }
+    
+    func logout(session: URLSession, completionHandler: @escaping (NetworkAction.Result) -> Void) {
+        print("Logout for \(self).")
+        configuration.logoutAction.commit(placeholders: placeholders, session: session, completionHandler: completionHandler)
+    }
+}
