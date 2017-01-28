bb13b1020acf132e402a424f3b7e30909a697e5d
Sihan Li <lisihan969@gmail.com>
2017-01-29 05:55:29 +0800

    Request user notification auth

--- CampNet iOS/AppDelegate.swift
+++ CampNet iOS/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            if granted {
+                print("User notifications are allowed.")
+            } else {
+                print("User notifications are not allowed. Error:", error as Any)
+            }
+        }
+
         return true
     }
 
