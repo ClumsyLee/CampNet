1f07a8125a178ae7720507c0c9f4d34975b2ff83
Sihan Li <lisihan969@gmail.com>
2017-08-30 03:34:12 -0700

    Revert "Hide name in logs."
    This reverts commit 7b4b74cd4d61e21f8a3a8b207b7efc5cf31574b2.

--- CampNetKit/Models/Configuration.swift
+++ CampNetKit/Models/Configuration.swift
@@ -192,10 +192,6 @@ public struct Profile {
 
 extension Profile: CustomStringConvertible {
     public var description: String {
-        var vars = self.vars
-        if let name = vars["name"] as? String {
-            vars["name"] = String(repeating: "*", count: name.characters.count)  // Hide name in logs.
-        }
         return vars.description
     }
 }
