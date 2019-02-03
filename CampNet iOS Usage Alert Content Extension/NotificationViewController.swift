9e7922ace441cac6870eae560c4d3564405a8adc
Sihan Li <lisihan969@gmail.com>
2019-02-04 02:53:52 +0800

    Add Usage Alert Content Extension

---
+++ CampNet iOS Usage Alert Content Extension/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  CampNet iOS Usage Alert Content Extension
+//
+//  Created by Thomas Lee on 2/4/19.
+//  Copyright © 2019 Sihan Li. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
