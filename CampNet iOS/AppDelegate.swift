cd8a3ec81873d5b0fddaf9b8d68a4d2cebfc2b1d
Sihan Li <lisihan969@gmail.com>
2019-03-16 23:48:32 -0700

    Make fewer donation requests (every 30 logins)

--- CampNet iOS/AppDelegate.swift
+++ CampNet iOS/AppDelegate.swift
@@ -25,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     static let bannerDuration = 3.0
     static let loginErrorNotificationInterval: TimeInterval = 86400
-    public static let donateRequestInterval = 25  // In terms of auto-login count.
+    public static let donateRequestInterval = 30  // In terms of auto-login count.
     public static let donateRequestMinInterval: TimeInterval = 7 * 86400
     public static let donationRequestIdentifier = "donationRequest"
 
